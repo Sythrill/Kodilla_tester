8d923b5b9dc032ab55c8168cba14138fbc0b31b2
Katarzyna Sciwiarska <ksciwiarska@gmail.com>
2019-11-19 22:05:38 +0100

    fix isLeap method

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -4,17 +4,22 @@ public class LeapYear {
     public LeapYear(int year) {
         this.year = year;
     }
+    public static void main(String[] args) {
+        LeapYear year = new LeapYear(2017);
+        Boolean a = year.isLeap();
+        Boolean b = year.isLeap2();
+        System.out.println(a);
+        System.out.println(b);
+    }
 
     public Boolean isLeap() {
         if (this.year % 4 == 0) {
-            return true;
-        } else if (this.year % 100 == 0) {
-            return true;
-        } else if (this.year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+            if (this.year % 100 == 0) {
+                if (this.year % 400 == 0){
+                    return true;
+                } else return false;
+            } else return true;
+        } else return false;
     }
 
 
