78565b8b88a6b4b48c071679bdc1634fc38cdbde
Katarzyna Sciwiarska <ksciwiarska@gmail.com>
2019-12-16 18:41:22 +0100

    add descriptions to tests

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -10,7 +10,10 @@ public class WarehouseTestSuite {
 
     @Test
     public void getOrderTest() throws OrderDoesntExistsException {
-
+        System.out.println("Test for method getOrder(). \n"
+                + "given - order numbers 1234, 432, 9876, 567890\n"
+                + "when - ask for order that exists\n"
+                + "then - return order number\n");
         warehouse.addOrder(new Order("1234"));
         warehouse.addOrder(new Order("432"));
         warehouse.addOrder(new Order("9876"));
@@ -21,7 +24,10 @@ public class WarehouseTestSuite {
 
     @Test(expected = OrderDoesntExistsException.class)
     public void getOrderTest_withException() throws OrderDoesntExistsException {
-
+        System.out.println("Test for exception in method getOrder(). \n"
+        + "given - order numbers 1234, 432, 9876, 567890\n"
+        + "when - ask for order that doesn't exists\n"
+        + "then - throw OrderDoesntExistsException\n");
         warehouse.addOrder(new Order("1234"));
         warehouse.addOrder(new Order("432"));
         warehouse.addOrder(new Order("9876"));
