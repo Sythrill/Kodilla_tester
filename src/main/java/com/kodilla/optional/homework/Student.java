a57a56f078faa7eed2abe6e7b9b939bae16cbe5c
Katarzyna Sciwiarska <ksciwiarska@gmail.com>
2019-12-16 18:59:42 +0100

    scecond constructor

--- src/main/java/com/kodilla/optional/homework/Student.java
+++ src/main/java/com/kodilla/optional/homework/Student.java
@@ -1,12 +1,20 @@
 package com.kodilla.optional.homework;
 
+import java.util.Optional;
+
 public class Student {
     private String studentName;
-    private Teacher teacher;
+    private Teacher teacher1;
+    private Optional<Object> teacher;
 
     public Student(String studentName, Teacher teacher) {
         this.studentName = studentName;
-        this.teacher = teacher;
+        this.teacher = Optional.ofNullable(teacher);
+    }
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+        this.teacher = Optional.empty();
     }
 
     public String getStudentName() {
@@ -14,6 +22,6 @@ public class Student {
     }
 
     public Teacher getTeacher() {
-        return teacher;
+        return teacher1;
     }
 }
