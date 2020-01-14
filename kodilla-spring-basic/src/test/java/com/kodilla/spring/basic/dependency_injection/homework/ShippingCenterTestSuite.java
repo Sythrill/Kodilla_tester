f03689ca7fe1355a9e19b4b92a8ad1215c42d27a
Katarzyna Sciwiarska <ksciwiarska@gmail.com>
2020-01-14 21:12:17 +0100

    add descriptions to tests

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/dependency_injection/homework/ShippingCenterTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/dependency_injection/homework/ShippingCenterTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -9,18 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ShippingCenterTestSuite {
+    String address = "Mountain Hill 12, New York";
     ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.dependency_injection");
     ShippingCenter bean = context.getBean(ShippingCenter.class);
 
     @Test
     public void shouldSendPackage() {
-        String myPackage = bean.sendPackage("Mountain Hill 12, New York", 22.4);
-        assertEquals("Package delivered to: Mountain Hill 12, New York", myPackage);
+        String myPackage = bean.sendPackage(address, 22.4);
+        System.out.println("Delivery address: " + address + " package weight: 22.4");
+        assertEquals("Package delivered to: " + address, myPackage);
     }
 
     @Test
     public void shouldNotSendPackage() {
-        String myPackage = bean.sendPackage("Mountain Hill 12, New York", 44.4);
-        assertEquals("Package not delivered to: Mountain Hill 12, New York", myPackage);
+        String myPackage = bean.sendPackage(address, 44.4);
+        System.out.println("Delivery address: " + address + " package weight: 44.4");
+        assertEquals("Package not delivered to: " + address, myPackage);
     }
 }
