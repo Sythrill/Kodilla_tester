35635dea59f661dccdc9c356aab816fbdcf1e6dd
Sythrill <ksciwiarska@gmail.com>
2020-01-18 22:39:06 +0100

    rest api homework

--- kodilla-rest-api/src/main/java/com/kodilla/rest/kodillarestapi/domain/BookDto.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/kodillarestapi/domain/BookDto.java
@@ -1,5 +1,7 @@
 package com.kodilla.rest.kodillarestapi.domain;
 
+import java.util.Objects;
+
 public class BookDto {
     private String title;
     private String author;
@@ -19,5 +21,19 @@ public class BookDto {
 
     public String getAuthor() {
         return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookDto bookDto = (BookDto) o;
+        return Objects.equals(title, bookDto.title) &&
+                Objects.equals(author, bookDto.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
     }
 }
