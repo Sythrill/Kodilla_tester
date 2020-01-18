3dea06e1809c2a0880249e0aa5df21a37f52463c
Sythrill <ksciwiarska@gmail.com>
2020-01-18 22:30:55 +0100

    rest api classwork

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/kodillarestapi/KodillaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.rest.kodillarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
